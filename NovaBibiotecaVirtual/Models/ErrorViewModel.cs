1a390cd5e2db2a2fda859d8609d7ece2bb17e844
kaueleao <kaue44@yahoo.com.br>
2022-05-31 22:43:59 -0300

    inicio-projeto

---
+++ NovaBibiotecaVirtual/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NovaBibiotecaVirtual.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
